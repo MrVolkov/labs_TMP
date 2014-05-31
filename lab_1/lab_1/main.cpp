9b61f19ad1fef915a016084717f1a3786a8c0156
MrVolkov <volkovvalery@MacBook-Air-volkov.local>
2014-05-31 21:30:06 +0400

    TMP
    Changes

---
+++ lab_1/lab_1/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"HELLO WORLD"<<endl;
+//<<<<<<< HEAD
+	cout<<"commited in the second branch";
+////=======
+	cout<<"commited in the current branch";
+///>>>>>>> origin/second_Branch
+	system("pause");
+	return 0;
+}
